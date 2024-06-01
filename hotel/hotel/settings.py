646bf3ba61599a2f2d9d8e539d58fe41eae81daf
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-06-01 14:51:15 +0000

    set up

--- hotel/hotel/settings.py
+++ hotel/hotel/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-z5v05i9!+=vgmao868uu4(-jg!c7u2k867t%s*1*@@g&$wf-s!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['8000-nielmc-django-project-0kylrta3cs.us2.codeanyapp.com']
 
 
